0ef23d14206a28776935586a76ee175199f82860
tawiah <tawiahin4k@gmail.com>
2025-02-19 10:52:20 +0000

    Update package-lock.json and add Typewriter component to enhance text display

--- resume-data/data.tsx
+++ resume-data/data.tsx
@@ -1,69 +1,99 @@
 export const BLUR_DELAY=0.04
 
 export const DATA = {
-  personalData:{
-    name:"tawiah",
-    description:"I am a Developer. I love to learn and explore new technologies and am passionate about problem-solving.",
-    skill:["Python", 'Data science', "Machine Learning", 'Cyber Security', 'Javascript', 'TypeScript',"Node.js","React.js","Next.js", "Express.js","MongoDB","Docker",'GraphQL'],
-    email:"tawiahin4k@gmail.com",
-    phone:"+233 508968239",
-    link:[
+  personalData: {
+    name: "tawiah",
+    description:
+      "I am a  Developer. I love to learn and explore new technologies and am passionate about problem-solving. I love to do most of the things using Python.",
+    skill: [
+      "Python",
+      "Flask",
+      "FastAPI",
+      "DJango",
+      "Data Science",
+      "Machine Learning",
+      "Deep Learning",
+      "Javascript",
+      "TypeScript",
+      "Node.js",
+      "React.js",
+      "Next.js",
+      "Express.js",
+      "MongoDB",
+      "Docker",
+      "PostgreSQL",
+      "Tailwind CSS",
+      "Framer Motion",
+      "REST API",
+      "CI/CD",
+      "Git",
+      "Github",
+      "Vercel",
+      "Netlify",
+      "AWS",
+      "Google Cloud",
+      "Firebase",
+      "OAuth",
+    ],
+    email: "davidtawiah.sh@gmail.com",
+    phone: "+233 508968239",
+    link: [
       {
-        name:"github",
-        url:"https://github.com/tawiahnyt/"
+        name: "github",
+        url: "https://github.com/tawiahnyt",
       },
       {
-        name:"linkedin",
-        url:"https://www.linkedin.com/in/david-tawiah"
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/david-tawiah",
       },
       {
-        name:"x",
-        url:"https://x.com/im_yogesh88"
+        name: "x",
+        url: "https://x.com/themilkyway",
       },
       {
-        name:"instagram",
-        url:"https://instagram.com/im.yogesh_"
+        name: "instagram",
+        url: "https://instagram.com/__tawiah",
       },
     ],
     resume: "/SinghYogesh.pdf",
-    aboutMe:`I'm a 25 y/o belonging to the Homo sapiens species 🤣 — a brother, a son, a friend, and a self-taught web developer and programmer. I have completed my **MCA (Master of Computer Applications)**, but my programming journey started much earlier. Back in the second year of my BSc (because my first year passed in sports 😁). My web development journey began with frontend development, using only **HTML**, **CSS**, and simple **JavaScript**. If you're curious about the technologies I know, here&apos;s the list: - **Python** (❤️) - **JavaScript** - Frameworks: **React.js**, **Next.js**, **Tailwind CSS** - **MERN Stack** (Currently Learning 🚀) Apart from coding and programming, I enjoy: - Playing **cricket** (🏏) — because touching grass is important! - Listening to **soft music** while watching clouds drift by 😳 - Playing mobile games like **Battle Royale** games When I&apos;m not doing any of the above. 
+    aboutMe: `I'm a 25 y/o belonging to the Homo sapiens species 🤣 — a brother, a son, a friend, and a self-taught web developer and programmer. I have completed my **MCA (Master of Computer Applications)**, but my programming journey started much earlier. Back in the second year of my BSc (because my first year passed in sports 😁). My web development journey began with frontend development, using only **HTML**, **CSS**, and simple **JavaScript**. If you're curious about the technologies I know, here&apos;s the list: - **Python** (❤️) - **JavaScript** - Frameworks: **React.js**, **Next.js**, **Tailwind CSS** - **MERN Stack** (Currently Learning 🚀) Apart from coding and programming, I enjoy: - Playing **cricket** (🏏) — because touching grass is important! - Listening to **soft music** while watching clouds drift by 😳 - Playing mobile games like **Battle Royale** games When I&apos;m not doing any of the above. 
 
 ---
 That&apos;s pretty much everything about me.  
-`
+`,
   },
   educations: [
     {
       id: 1,
       year: "2020-2024",
-      title:"Master of Computer Applications (MCA)",
-      school:"Noida Institute Of Engineering And Technology,Greater Noida",
-      location:"UP. Greater Noida",
-      field:"Computer Application"
+      title: "Master of Computer Applications (MCA)",
+      school: "Noida Institute Of Engineering And Technology,Greater Noida",
+      location: "UP. Greater Noida",
+      field: "Computer Application",
     },
     {
       id: 2,
-      year:"2017 - 2020",
-      title:"Bachelor of Science (B.Sc IT)",
-      school:"Anand Vishwa Gurukul Sr. Night College Thane West Maharashtr",
-      location:"Thane, Maharashtra",
-      field:"Information Technology"
+      year: "2017 - 2020",
+      title: "Bachelor of Science (B.Sc IT)",
+      school: "Anand Vishwa Gurukul Sr. Night College Thane West Maharashtr",
+      location: "Thane, Maharashtra",
+      field: "Information Technology",
     },
     {
       id: 3,
       year: "2015 - 2017",
-      title:"Higher Secondary Certificate(HSC)",
-      school:"Gaynodya Vidya Mandir Junior College Thane(W),Maharashtra",
-      location:"Thane, Maharashtra",
-      field:"Science"
+      title: "Higher Secondary Certificate(HSC)",
+      school: "Gaynodya Vidya Mandir Junior College Thane(W),Maharashtra",
+      location: "Thane, Maharashtra",
+      field: "Science",
     },
     {
       id: 4,
       year: "2014 - 2015",
-      title:"Secondary School Certificate(SSC)",
-      school:"Gaynodya Vidya Mandir Junior College Thane(W),Maharashtra",
-      location:"Thane, Maharashtra",
-      field:"Regular"
+      title: "Secondary School Certificate(SSC)",
+      school: "Gaynodya Vidya Mandir Junior College Thane(W),Maharashtra",
+      location: "Thane, Maharashtra",
+      field: "Regular",
     },
   ],
   experiences: [
@@ -73,23 +103,41 @@ That&apos;s pretty much everything about me.
       startDate: "Nov 2024",
       endDate: "Jan 2025",
       location: "Thane, IN",
-      technologies: ["Next.js", "TypeScript", "Tailwind CSS","Express.js","Mongodb", "Shadcn UI", "Zustand"]
+      technologies: [
+        "Next.js",
+        "TypeScript",
+        "Tailwind CSS",
+        "Express.js",
+        "Mongodb",
+        "Shadcn UI",
+        "Zustand",
+      ],
     },
     {
-        company: "Weingenious Technocrats Private Ltd",
-        role: "Frontend Developer",
-        startDate: "Nov 2022",
-        endDate: "Oct 2024",
-        location: "Surat, IN",
-        technologies: ["React.js", "Next.js","Tailwind CSS", "Redux", "Zustand","Material UI","Git", "CI/CD", "Graphql"]
+      company: "Weingenious Technocrats Private Ltd",
+      role: "Frontend Developer",
+      startDate: "Nov 2022",
+      endDate: "Oct 2024",
+      location: "Surat, IN",
+      technologies: [
+        "React.js",
+        "Next.js",
+        "Tailwind CSS",
+        "Redux",
+        "Zustand",
+        "Material UI",
+        "Git",
+        "CI/CD",
+        "Graphql",
+      ],
     },
     {
-        company: "Cloud Analogy Private Ltd",
-        role: "Salesforce developer Intern",
-        startDate: "Feb 2022",
-        endDate: "Apr 2022",
-        location: "Noida, IN",
-        technologies: ["Java","Apex lightning","SOQL"]
+      company: "Cloud Analogy Private Ltd",
+      role: "Salesforce developer Intern",
+      startDate: "Feb 2022",
+      endDate: "Apr 2022",
+      location: "Noida, IN",
+      technologies: ["Java", "Apex lightning", "SOQL"],
     },
   ],
   projectData: [
@@ -101,23 +149,26 @@ That&apos;s pretty much everything about me.
         { title: "You can copy and paste into your web apps." },
       ],
       link: "https://www.shapexui.site",
-      code:"https://github.com/yogesh2104/shapex-ui",
-      image: '/project/shapexui.png',
-      tools: ['Next.js', 'Framer Motion', 'Tailwind CSS', 'Shadcn/ui'],
-      role: 'Maintainer',
+      code: "https://github.com/yogesh2104/shapex-ui",
+      image: "/project/shapexui.png",
+      tools: ["Next.js", "Framer Motion", "Tailwind CSS", "Shadcn/ui"],
+      role: "Maintainer",
     },
     {
       name: "Debate-AI",
       title: "Debate-AI",
       results: [
         { title: "Developed an AI-powered debate platform." },
-        { title: "Debate summaries with performance scores, resource recommendations," },
+        {
+          title:
+            "Debate summaries with performance scores, resource recommendations,",
+        },
       ],
       link: "https://debate-arena.vercel.app/",
-      code:"",
-      image: '/project/debate.png',
-      tools: ['NextJs 15', 'MongoDB(Prisma ORM)', 'AuthJs'],
-      role: 'Full Stack Developer',
+      code: "",
+      image: "/project/debate.png",
+      tools: ["NextJs 15", "MongoDB(Prisma ORM)", "AuthJs"],
+      role: "Full Stack Developer",
     },
     {
       name: "Noter",
@@ -127,10 +178,10 @@ That&apos;s pretty much everything about me.
         { title: "Where enhanced and more efficient work takes place." },
       ],
       link: "https://noter-steel.vercel.app/",
-      code:"https://github.com/yogesh2104/noter",
-      image: '/project/noter.png',
-      tools: ['NextJs', 'ConvexDB', 'Clerk Auth'],
-      role: 'Full Stack Developer'
+      code: "https://github.com/yogesh2104/noter",
+      image: "/project/noter.png",
+      tools: ["NextJs", "ConvexDB", "Clerk Auth"],
+      role: "Full Stack Developer",
     },
     {
       name: "PromptForAi",
@@ -141,37 +192,40 @@ That&apos;s pretty much everything about me.
         { title: "Also you can Search other people prompt" },
       ],
       link: "https://prompt-for-ai.vercel.app/",
-      image: '/project/promtForAi.png',
-      tools: ['NextJs', 'MongoDB', 'NextAuth'],
-      role: 'Full Stack Developer',
-      code: 'https://github.com/yogesh2104/promptForAI'
+      image: "/project/promtForAi.png",
+      tools: ["NextJs", "MongoDB", "NextAuth"],
+      role: "Full Stack Developer",
+      code: "https://github.com/yogesh2104/promptForAI",
     },
     {
-        name: "Smart Health Advisor",
-        title: "Predicts Diseases Based On Symptoms",
-        results: [
-          { title: "Discover AI Prompt and Share AI" },
-          { title: "Create new prompt" },
-          { title: "Also you can Search other people prompt" },
-        ],
-        link: "https://smart-health-advisor.vercel.app/",
-        image: '/project/health.png',
-        tools: ['NextJs', 'Shadcn UI', 'Django','Python'],
-        role: 'Frontend Developer',
-        code: 'https://github.com/saurabhsharma104/Smart-Health-Advisor',
-    }, 
+      name: "Smart Health Advisor",
+      title: "Predicts Diseases Based On Symptoms",
+      results: [
+        { title: "Discover AI Prompt and Share AI" },
+        { title: "Create new prompt" },
+        { title: "Also you can Search other people prompt" },
+      ],
+      link: "https://smart-health-advisor.vercel.app/",
+      image: "/project/health.png",
+      tools: ["NextJs", "Shadcn UI", "Django", "Python"],
+      role: "Frontend Developer",
+      code: "https://github.com/saurabhsharma104/Smart-Health-Advisor",
+    },
     {
-        name: "Coding InterView",
-        title: "Coding-Interview-University Repo.",
-        results: [
-          { title: "This is simple static web page" },
-          { title: "Simplified version of coding-interview-university github repo." },
-        ],
-        link: "https://yogesh2104.github.io/CodingInterView/",
-        image: '/project/iViewer.png',
-        tools: ['HTML', 'CSS','Javascript', 'Bootstrap'],
-        role: 'Frontend Developer',
-        code: 'https://github.com/yogesh2104/CodingInterView',
+      name: "Coding InterView",
+      title: "Coding-Interview-University Repo.",
+      results: [
+        { title: "This is simple static web page" },
+        {
+          title:
+            "Simplified version of coding-interview-university github repo.",
+        },
+      ],
+      link: "https://yogesh2104.github.io/CodingInterView/",
+      image: "/project/iViewer.png",
+      tools: ["HTML", "CSS", "Javascript", "Bootstrap"],
+      role: "Frontend Developer",
+      code: "https://github.com/yogesh2104/CodingInterView",
     },
     {
       name: "ThreeJs Diamond",
@@ -181,10 +235,10 @@ That&apos;s pretty much everything about me.
         { title: "Allow to change ring and diamond color" },
       ],
       link: "https://threejsdesign.vercel.app/",
-      image: '/project/threejs.png',
-      tools: ['Nextjs','Javascript', 'ThreeJS', 'Zustand', 'Tailwind CSS'],
-      role: 'Frontend Developer',
+      image: "/project/threejs.png",
+      tools: ["Nextjs", "Javascript", "ThreeJS", "Zustand", "Tailwind CSS"],
+      role: "Frontend Developer",
       code: null,
-    }, 
+    },
   ],
 };
